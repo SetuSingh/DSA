a6791d75fa7c07455c12466ce753aaa48bd41730
SetuSingh <setuawesome001@gmail.com>
2024-05-30 11:02:25 +0530

    feat: prime-factor optimised

--- Mathematics/prime.js
+++ Mathematics/prime.js
@@ -6,22 +6,14 @@ const prime = (num)=>{
         return false
     }
 
-    for (var i=5; i*i <= num; i+=6){
-        if (num%i == 0){
+    for (var i=5; i*i <= num; i+=6){ 
+        if (num%i == 0 || num%(i+2) == 0){
             return false
         }
     }
 
     return true
 }
-const testNumbers = [1, 2, 3, 4, 5, 7, 9, 11, 15, 17, 19, 23, 25, 29, 31, 37, 41, 43, 47, 51, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-testNumbers.forEach(num => {
-    const isPrime = prime(num)
-    if(isPrime){
-        console.log(`${num} a prime number`);
-    }
-    
-});
 
-console.log(prime(65))
+console.log(prime(5))
